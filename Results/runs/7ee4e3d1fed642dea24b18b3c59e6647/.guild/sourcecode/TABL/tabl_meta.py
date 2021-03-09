2b16ff2331efebf429a0d1f2b7f9230200c46593
blepo <bl@eng.au.dk>
2021-03-09 17:30:47 +0100

    Reimplemented TABL

---
+++ Results/runs/7ee4e3d1fed642dea24b18b3c59e6647/.guild/sourcecode/TABL/tabl_meta.py
@@ -0,0 +1,29 @@
+import tensorflow as tf
+import tensorflow.keras as k
+
+
+class Params:
+    """
+    Class containing all required parameters for its classifier
+    """
+
+    def __init__(self):
+        self.a = [[60, 10], [40, 1], [4, 1]]
+        self.b = [[120, 5], [60, 2], [4, 1]]
+        self.c = [[240, 5], [120, 1], [4, 1]]
+        self.d = [[240, 5], [120, 2], [4, 1]]
+        self.e = [[240, 5], [120, 2], [60, 1], [4, 1]]
+
+        self.param_tabl_1 = {'template': self.b,
+                             'dropout': 0.1,
+                             'projection_regularizer': None,
+                             'projection_constraint': k.constraints.max_norm(3.0, axis=0),
+                             'attention_regularizer': None,
+                             'attention_constraint': k.constraints.max_norm(5.0, axis=1),
+                             'batch_size': 512,
+                             'classifier_epochs': 100,
+                             'learning_rate': 0.001,
+                             'optimizer': tf.keras.optimizers.Adam(),
+                             'loss_function': tf.keras.losses.CategoricalCrossentropy()}
+
+        self.params_list = [self.param_tabl_1]
